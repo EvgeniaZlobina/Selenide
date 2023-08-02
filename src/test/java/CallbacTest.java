a7209ddd286a4ceac70b86be47e4e87d8b32cb1d
EvgeniaZlobina <ZEA_6590@mail.ru>
2023-08-02 22:05:23 +0500

    First commit

---
+++ src/test/java/CallbacTest.java
@@ -0,0 +1,22 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CallbacTest {
+
+    @Test
+    void orderacardwithdelivery() {
+        open ("http://localhost:9999");
+
+    }
+
+}
