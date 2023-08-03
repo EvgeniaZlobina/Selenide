c1711c86955abd965f3aebb59c3ae5022f42e027
EvgeniaZlobina <ZEA_6590@mail.ru>
2023-08-03 20:27:39 +0500

    implementation of autotests

--- src/test/java/CallbacTest.java
+++ src/test/java/CallbacTest.java
@@ -1,21 +1,43 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbacTest {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
 
     @Test
     void orderacardwithdelivery() {
-        open ("http://localhost:9999");
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Пермь");
+        String currentDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").sendKeys(currentDate);
+        $("[data-test-id='name'] input").setValue("Иванов Захар");
+        $("[data-test-id='phone'] input").setValue("+78982000000");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15))
+       .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
 
     }
 
